f87e7ce03a1bddef4a0e1f5c3d3a56a4ac71877c
delvinablakaj10 <d.blakaj1108@gmail.com>
2024-07-16 14:40:38 +0200

    Create a poll,add choices,vote, add users with djoser

---
+++ polls/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+
+from .apiviews import ChoiceList, CreateVote, PollDetail, PollList
+
+# from .views import polls_list, polls_detail
+
+# urlpatterns = [
+#     path("polls/", polls_list, name="polls_list"),
+#     path("polls/<int:pk>/", polls_detail, name="polls_detail")
+# ]
+
+
+
+urlpatterns = [
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
+]
